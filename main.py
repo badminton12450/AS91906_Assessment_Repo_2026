bf7e04084c8f29af6f85c360058a08bb08632eec
Jon-StPats <jon.bennett@stpats.school.nz>
2026-03-03 10:33:54 +1300

    add menu.py

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+AS91906 Assessment Scaffold
+Complete this file by developing your full program.
+
+You must:
+- Organise your program using functions
+- Use at least TWO complex programming techniques
+- Include input validation
+- Include testing evidence (separate file)
+"""
+
+def main():
+    print("Start building your assessment program here.")
+
+    # TODO:
+    # 1. Create your data structures
+    # 2. Build your main menu (if text-based)
+    # 3. Call your functions
+
+
+if __name__ == "__main__":
+    main()
